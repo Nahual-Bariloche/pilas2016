0e0d24b7a0e6b8c801793d7ba59cbe958a57a869
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-09-13 16:38:03 -0300

    base para juegos tipo mario bross + readme

---
+++ base_mario.py
@@ -0,0 +1,45 @@
+import pilasengine
+
+pilas = pilasengine.iniciar()
+
+class Protagonista(pilasengine.actores.Actor):
+
+    def iniciar(self):
+        self.imagen = "aceituna.png"
+        self.figura = pilas.fisica.Circulo(self.x, self.y, 17)
+
+#        self.figura.sin_rotacion = True
+        self.figura.escala_de_gravedad = 2
+
+        self.sensor_pies = pilas.fisica.Rectangulo(self.x, self.y, 20, 5, dinamica=False)
+
+    def actualizar(self):
+        velocidad = 10
+        salto = 15
+        self.x = self.figura.x
+        self.y = self.figura.y
+
+        if self.pilas.control.derecha:
+            self.figura.velocidad_x = velocidad
+        elif self.pilas.control.izquierda:
+            self.figura.velocidad_x = -velocidad
+        else:
+            self.figura.velocidad_x = 0
+
+        if self.esta_pisando_el_suelo():
+            if self.pilas.control.arriba and int(self.figura.velocidad_y) <= 0:
+                self.figura.impulsar(0, salto)
+
+        self.sensor_pies.x = self.x
+        self.sensor_pies.y = self.y - 20
+        
+    def esta_pisando_el_suelo(self):
+        return len(self.sensor_pies.figuras_en_contacto) > 0
+
+mapa = pilas.actores.MapaTiled('mapa_desde_archivo.tmx')
+
+pilas.actores.vincular(Protagonista)
+p = pilas.actores.Protagonista()
+
+
+pilas.ejecutar()
